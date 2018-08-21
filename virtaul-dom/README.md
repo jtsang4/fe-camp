dee77163b02e56e7cbff658298a4295dd7b96313
wtzeng <wentao_zeng1@163.com>
2018-08-21 15:50:28 +0800

    implement updateRealDOM

--- virtaul-dom/README.md
+++ virtaul-dom/README.md
@@ -1,3 +1,7 @@
 # how to write your own virtual dom
 
-Original article: [how to write your own virtual dom](https://medium.com/@deathmood/how-to-write-your-own-virtual-dom-ee74acc13060)
+> Original article: [how to write your own virtual dom](https://medium.com/@deathmood/how-to-write-your-own-virtual-dom-ee74acc13060)
+
+A mini version of virtual dom.
+
+*Has not yet add babel to transform JSX.*
--- virtaul-dom/main.jsx
+++ virtaul-dom/main.jsx
@@ -1,5 +1,5 @@
 /**
- * Generate virtual dom object - used by babel
+ * Generate virtual dom object (used by babel).
  * @param type - node type
  * @param props - node props
  * @param children - childNodes
@@ -9,37 +9,120 @@ function h(type, props, ...children) {
   return {type, props, children}
 }
 
-/** @jsx: h */
+window.onload = () => {
+  /** @jsx h */
 
 // jsx element for test
-const elem = (
-  <ul className="list">
-    <li>item 1</li>
-    <li>item 2</li>
-  </ul>
-)
+  const $elem = (
+    <ul className="list">
+      <li>item 1</li>
+      <li>item 2</li>
+    </ul>
+  )
 
-/**
- * Create real dom node by virtual dom node
- * @param node - VM node
- * @returns {Text|HTMLElement}
- */
-function createRealDOM(node) {
-  if (typeof node === 'string') {
-    return document.createTextNode(node)
-  } else  {
-    const element = document.createElement(node.type)
-    const props = node.props || {}
-    const children = node.children || []
+  /**
+   * Create real dom node by virtual dom node.
+   * @param node - VM node
+   * @returns {Text|HTMLElement}
+   */
+  function createRealDOM(node) {
+    if (typeof node === 'string') {
+      return document.createTextNode(node)
+    } else  {
+      const $element = document.createElement(node.type)
+      const props = node.props || {}
+      const children = node.children || []
 
-    Object.keys(props).forEach(attr => {
-      element.setAttribute(attr, node.props[attr])
-    })
-    children.map(createRealDOM).forEach(element.appendChild.bind(element))
+      Object.keys(props).forEach(attr => {
+        $element.setAttribute(attr, node.props[attr])
+      })
+      children.map(createRealDOM).forEach($element.appendChild.bind($element))
 
-    return element
+      return $element
+    }
   }
-}
 
 // Case 1: generate real dom by virtual dom
-console.log(createRealDOM(elem))
+  const $root = document.getElementById('root')
+  $root.appendChild(createRealDOM($elem))
+
+  /**
+   * Update children for a specific parent node.
+   * For specific parent node, there are several cases of updating, 1. add node; 2. delete node; 3. replace node; 4. diff children of child node.
+   * @param {HTMLElement} $parent - parent element node
+   * @param newNode - child node at next state
+   * @param oldNode - child node at previous state
+   * @param {Number} index - current child node's index
+   */
+  function updateRealDOM($parent, newNode, oldNode, index = 0) {
+    // it's impossible that at arbitrary valid index, oldNode and newNode are both undefined. so if one is undefined, the other must be defined.
+    if (!oldNode) {
+      $parent.appendChild(createRealDOM(newNode))
+    } else if (!newNode) {
+      $parent.removeChild($parent.childNodes[index])
+    } else if (nodeHasChanged(newNode, oldNode)) {
+      $parent.replaceChild(createRealDOM(newNode), $parent.childNodes[index])
+    } else if (newNode.type) { // only element node has children
+      const newChildrenLength = newNode.children.length
+      const oldChildrenLength = oldNode.children.length
+      for (let i = 0; i < Number.max(newChildrenLength, oldChildrenLength); i++) {
+        updateRealDOM($parent.childNodes[index], newNode.children[i], oldNode.children[i], i)
+      }
+    }
+  }
+
+  /**
+   * Judge if a child node at arbitrary index is updated.
+   * @param newNode
+   * @param oldNode
+   * @returns {boolean}
+   */
+  function nodeHasChanged(newNode, oldNode) {
+    return (typeof newNode !== typeof oldNode) ||
+      (typeof oldNode === 'string' && newNode !== oldNode) ||
+      (newNode.type !== oldNode.type)
+  }
+
+// Case 2: update DOM
+  let currentState = $elem
+  const statePool = {
+    state: [
+      (
+        <ul className="list">
+          <li>item 1</li>
+          <li>item 2</li>
+          <li>item 3</li>
+        </ul>
+      ),
+      (
+        <ul className="list">
+          <li>item 2</li>
+          <li>item 3</li>
+        </ul>
+      ),
+      (
+        <ul className="list">
+          <li>item 2</li>
+          <div>div item</div>
+        </ul>
+      ),
+      (
+        <ul className="list">
+          <li>item 2</li>
+          <div>
+            <ul className="list">
+              <li>item 4</li>
+            </ul>
+          </div>
+        </ul>
+      )
+    ],
+    currentIndex: 0
+  }
+  document.getElementById('btn').addEventListener('click', () => {
+    const nextState = statePool.state[statePool.currentIndex % 4]
+    updateRealDOM($root, nextState, currentState)
+    currentState = nextState
+    statePool.currentIndex++
+  })
+}
