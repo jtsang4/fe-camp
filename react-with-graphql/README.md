84fe186f45cad9b3a38ded6d2a6ada910dd09f9b
wtzeng <wentao_zeng1@163.com>
2018-10-15 22:46:27 +0800

    start a new practice of react-with-graphql

---
+++ react-with-graphql/README.md
@@ -0,0 +1,3 @@
+# React with GraphQL
+
+Practice for [Build an Online Store with React and GraphQL in 90 Minutes](https://www.udemy.com/build-an-online-store-with-react-and-graphql-in-90-minutes/learn/v4/overview)
---
+++ react-with-graphql/client/src/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+import Signin from './components/Signin'
+import Signup from './components/Signup'
+import Checkout from './components/Checkout'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import * as serviceWorker from './serviceWorker'
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route component={App} exact path="/" />
+      <Route component={Signin} path="/signin" />
+      <Route component={Signup} path="/signup" />
+      <Route component={Checkout} path="/checkout" />
+    </Switch>
+  </Router>
+)
+
+ReactDOM.render(<Root />, document.getElementById('root'))
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister()
