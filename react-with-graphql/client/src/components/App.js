b3bb34d5a390183b8710eb29d05ad6e7042aef5d
wtzeng <wentao_zeng1@163.com>
2018-11-04 01:58:23 +0800

    lesson21

--- react-with-graphql/client/src/components/App.js
+++ react-with-graphql/client/src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Container, Box, Heading, Card, Image, Text } from 'gestalt';
+import { Container, Box, Heading, Card, Image, Text, SearchField, Icon } from 'gestalt';
 import { Link } from 'react-router-dom'
 import Strapi from 'strapi-sdk-javascript/build/main';
 import './App.css';
@@ -9,7 +9,12 @@ const strapi = new Strapi(apiUrl);
 
 class App extends Component {
   state = {
-    brands: []
+    brands: [],
+    searchTerm: '',
+  }
+
+  handleChange = (event) => {
+    this.setState({ searchTerm: event.value });
   }
 
   async componentDidMount() {
@@ -32,15 +37,32 @@ class App extends Component {
         }
       });
 
-      this.setState({ brands: response.data.brands })
+      this.setState({ brands: response.data.brands });
     } catch (err) {
-      console.error(err)
+      console.error(err);
     }
   }
 
   render() {
     return (
       <Container>
+        {/* Brands search fields */}
+        <Box display="flex" justifyContent="center" marginTop={4}>
+          <SearchField
+            id="searchField"
+            accessibilityLabel="Brands Search Field"
+            onChange={this.handleChange}
+            placeholder="Search Brands"
+          />
+          <Box margin={2}>
+            <Icon
+              icon="filter"
+              color={this.state.searchTerm ? 'orange' : 'grey'}
+              size={20}
+              accessibilityLabel="Filter"
+            />
+          </Box>
+        </Box>
         {/* Brands section */}
         <Box
           display="flex"
@@ -54,14 +76,22 @@ class App extends Component {
         </Box>
         {/* Brands */}
         <Box
+          dangerouslySetInlineStyle={{
+            __style: {
+              backgroundColor: '#d6c8ec'
+            }
+          }}
+          shape="rounded"
+          wrap
           display="flex"
           justifyContent="around"
         >
           {this.state.brands.map(brand => (
-            <Box margin={2} width={200} key={brand._id}>
+            <Box paddingY={4} margin={2} width={200} key={brand._id}>
               <Card
                 image={<Box height={200} width={200}>
                   <Image
+                    fit="cover"
                     alt="brand"
                     naturalWidth={1}
                     naturalHeight={1}
